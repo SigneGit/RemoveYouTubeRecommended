cf0d683be172016e2b34ca67bc5cf13440da7ddb
xzero213 <xzero213@gmail.com>
2016-12-23 17:02:04 -0600

    Add files via upload

--- RemoveYouTubeRecommended.js
+++ RemoveYouTubeRecommended.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Remove YouTube recommended videos
-// @namespace   /u/Signe_
+// @author      /u/Signe_
 // @description Hides youtube recommended for you in side bar 
 // @include     *.youtube.com/*
 // @version     1
@@ -9,44 +9,32 @@
 var SpanLength = - 1;
 var iRemovedVideos = 0;
 var _Counter = 0;
-var CurrentVideoHref = "";
-
+var CurrentVideoHref = '';
 var tDetectNewVideos;
-
 function SetupTimer() {
   tDetectNewVideos = setInterval(Detect_NewVideos, 1000);
-}
-//Detects when you click on a new video or the 'show more' button
-function Detect_NewVideos() { 
-  if (SpanLength == - 1)
-  {
-    console.log('[DetectVideos] No videos inside');
-    clearInterval(tDetectNewVideos);
-    return;
-  }
+} //Detects when you click on a new video or the 'show more' button
+
+function Detect_NewVideos() {
   var spans = document.getElementsByClassName('view-count');
   var Temp = window.location.href;
-  
-  
   if (spans.length > SpanLength || Temp != CurrentVideoHref && _Counter > 3)
   {
-    RemoveRecommended();
-    var RemovedVideos = iRemovedVideos;
-    console.log('[DetectVideos] Removed ' + RemovedVideos + ' new videos');
-    clearInterval(tDetectNewVideos);
+    if (window.location.href.includes('watch?v='))
+    {
+      RemoveRecommended();
+      var RemovedVideos = iRemovedVideos;
+      console.log('[DetectVideos] Removed ' + RemovedVideos + ' new videos');
+    }    //clearInterval(tDetectNewVideos);
   }
   _Counter++;
-  if (_Counter >= 10) { //if it takes more than 10 seconds abort
-    console.log("[DetectVideos] Cleared Timer after 10 seconds");
-    clearInterval(tDetectNewVideos);
-  }
-}
-//Actually removes the recommended videos from view
+} //Actually removes the recommended videos from view
+
 function RemoveRecommended() {
   iRemovedVideos = 0;
-  //CurrentVideoTitle = getContentByMetaTagName("og:title");
+  SpanLength = 0;
+  _Counter = 0;
   CurrentVideoHref = window.location.href;
-  
   var spans = document.getElementsByClassName('view-count');
   var searchText = 'Recommended for you';
   for (var i = 0; i < spans.length; i++)
@@ -56,20 +44,12 @@ function RemoveRecommended() {
       spans[i].parentNode.style.display = 'none';
       spans[i].textContent = '[DetectVideos] Removed';
       iRemovedVideos++;
-    } 
-    else
-    {
-      spans[i].parentNode.addEventListener('click', SetupTimer, false);
     }
     if (spans[i].innerHTML.includes(searchText))
     {
       spans[i].parentNode.style.display = 'none';
       spans[i].innerHTML = '[DetectVideos] Removed';
       iRemovedVideos++;
-    } 
-    else
-    {
-      spans[i].parentNode.addEventListener('click', SetupTimer, false);
     }
   }
   var WatchMore = document.getElementsByClassName('yt-uix-button');
@@ -85,4 +65,5 @@ function RemoveRecommended() {
   console.log('[DetectVideos] Removed ' + iRemovedVideos + ' Videos');
   console.log('[DetectVideos] Finished');
 }
-RemoveRecommended();
+SetupTimer();
+//RemoveRecommended();
